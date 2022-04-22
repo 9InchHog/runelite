29c175214ad0c0acf4dd46d9a0f63ded7821da76
9InchHog <96740390+9InchHog@users.noreply.github.com>
2022-04-22 07:34:49 -0500

    update

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -24,10 +24,10 @@
  */
 
 object ProjectVersions {
-    const val launcherVersion = "2.2.0"
-    const val rlVersion = "1.8.18.1"
+    const val launcherVersion = "2.2.5"
+    const val rlVersion = "1.8.18.2"
 
-    const val openosrsVersion = "4.24.1"
+    const val openosrsVersion = "4.25.0"
 
     const val rsversion = 204
     const val cacheversion = 165
